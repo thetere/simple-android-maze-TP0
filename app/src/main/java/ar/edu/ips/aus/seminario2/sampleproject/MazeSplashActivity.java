3cd10aa215a7870a580a100967a9eae5d7506e7f
thetere <tereseta@gmail.com>
2021-10-18 22:43:43 -0300

    splash screen modificado, se añade texto

--- app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/MazeSplashActivity.java
+++ app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/MazeSplashActivity.java
@@ -14,7 +14,7 @@ public class MazeSplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.maze_splash_screen);
 
         mHandler = new Handler();
         mRunnable = new Runnable() {
