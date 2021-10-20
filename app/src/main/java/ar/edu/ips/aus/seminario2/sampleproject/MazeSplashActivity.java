6e99d11fd5796b62bc873a92caa5f668819de8b9
thetere <tereseta@gmail.com>
2021-10-19 21:02:14 -0300

    agregue boton y cambie imagen y color de texto en splashactivity

--- app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/MazeSplashActivity.java
+++ app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/MazeSplashActivity.java
@@ -3,6 +3,8 @@ package ar.edu.ips.aus.seminario2.sampleproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +18,7 @@ public class MazeSplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maze_splash_screen);
 
-        mHandler = new Handler();
+        /*mHandler = new Handler();
         mRunnable = new Runnable() {
             @Override
             public void run() {
@@ -24,10 +26,22 @@ public class MazeSplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        };
+        };*/
 
         // its trigger runnable after 4000 millisecond.
-        mHandler.postDelayed(mRunnable,4000);
+        // mHandler.postDelayed(mRunnable,4000);
+
+        Button btnCallGame = (Button) findViewById(R.id.buttonJugar);
+        btnCallGame.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent elJuegoPosta = new Intent(MazeSplashActivity.this, MazeBoardActivity.class);
+                startActivity(elJuegoPosta);
+                finish();
+            }
+        });
+
 
     }
 
